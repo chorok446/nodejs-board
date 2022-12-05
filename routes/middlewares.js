0dd2eaa1cc3e3d198871f7c2fd86793528af125b
chorok446 <jana97@naver.com>
2022-12-05 21:00:22 +0900

    유효성 검사 추가중

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
@@ -13,4 +15,16 @@ exports.isNotLoggedIn = (req, res, next) => {
         const message = encodeURIComponent('로그인한 상태입니다.');
         res.redirect(`/?error=${message}`);
     }
+};
+
+
+
+exports.validationError = {
+    validateError: (req, res, next) => {
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            return next();
+        }
+        return res.status(400).json({ message: errors.array()[0].msg });
+    },
 };
